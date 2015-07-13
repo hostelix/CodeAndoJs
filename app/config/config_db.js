a7191a1edebf05924ad44bbe65f71131094f0209
Israel Lugo <hostelixisrael@gmail.com>
2015-07-13 03:31:51 -0430

    add file config/config_db.js and testing mongoose config ok

---
+++ app/config/config_db.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'uri_db': 'mongodb://root:123456@localhost:27017/codeandojs',
+};
--- run_app.js
+++ run_app.js
@@ -3,12 +3,26 @@ var express = require('express'),
 	mongoose = require('mongoose'),
 	bodyParser = require('body-parser'),
 	path = require('path'),
-	config = require('./app/config/config_path'),
+	config_path = require('./app/config/config_path'),
+	config_db = require('./app/config/config_db'),
 	app = express(),
 	swig = require('swig');
 
 
 PORT  = process.env.PORT || 3000;
+
+//Coneccet to mongoDB
+var db = mongoose.connection;
+	
+db.on('error', function(err){
+	console.log('Failure to connect-> '+err);
+});
+
+db.once('open', function() {
+  console.info('Connected to database');
+});
+
+mongoose.connect(config_db.uri_db);
 
 
 app.engine('html', swig.renderFile);
@@ -16,10 +30,10 @@ app.set('view engine', 'html');
 app.set('view cache', false);
 swig.setDefaults({ cache: false });
 
-app.set('views', __dirname+config.dir_views);
+app.set('views', __dirname+config_path.dir_views);
 
 
-app.use(express.static(config.dir_public));
+app.use(express.static(config_path.dir_public));
 
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -29,6 +43,8 @@ app.use(bodyParser.json());
 app.get('/',function(req,res){
 	res.render('index/index');
 })
+
+
 
 app.listen(PORT,function(){
 	console.log("Server Runnig In Port: "+ PORT);
